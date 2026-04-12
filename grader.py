0a807f38170669dd3a81a1794b2672b70e5405aa
likhitha-rk <likhithark7@gmail.com>
2026-04-12 18:16:26 +0530

    Update grader.py

--- grader.py
+++ grader.py
@@ -1,21 +1,32 @@
 def grade_easy(env):
-    if env.done:                # ✅ correct attribute
-        return 0.9
+    """
+    Easy task grading
+    """
+    if env.meeting_booked:
+        return 0.9   # NOT 1.0
     elif env.employee_id:
         return 0.5
     else:
         return 0.2
 
+
 def grade_medium(env):
-    if env.done:
+    """
+    Medium task grading
+    """
+    if env.meeting_booked:
         return 0.85
     elif env.calendar_checked:
         return 0.6
     else:
         return 0.3
 
+
 def grade_hard(env):
-    if env.done:
+    """
+    Hard task grading
+    """
+    if env.meeting_booked:
         return 0.8
     elif env.calendar_checked:
         return 0.55
